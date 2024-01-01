aecfcc2b394e21915bc78a492a69eb195d4584d9
周颂杰 <31345708+1noob@users.noreply.github.com>
2024-01-01 05:34:28 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[]}) => {
     <>
       <main className={`justify-center ${jetbrainsMono.variable} font-mono`}>
         <div className="my-auto overflow-hidden w-full h-dvh grid content-center">
-          <div className="animate-[scy_120s_linear_infinite] w-max grayscale-[50%]">
+          <div className="animate-[scy_60s_linear_infinite] w-max grayscale-[50%]">
               <div className="float-left grid grid-rows-8 grid-flow-col">
                 { images.map(({public_id, format }) => (
                     <Image
